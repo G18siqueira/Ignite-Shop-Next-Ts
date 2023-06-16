94612bea93087fac0f16576afd4ce7c1d2d416bf
Gustavo_Siqueira <gustavo@id360.com.br>
2023-06-15 22:43:33 -0300

    feat: Integração com Stripe - Utilizando SSG e formatação preço.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { stripe } from '@/lib/stripe'
 import { useKeenSlider } from 'keen-slider/react'
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl'
@@ -84,7 +84,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
@@ -95,13 +95,17 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     }
   })
-  console.log(products)
+
   return {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
